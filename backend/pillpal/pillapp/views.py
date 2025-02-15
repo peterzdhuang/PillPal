d75d017f18ef7404b7b0aac60e0a8bf6a1b47eab
Daniel Cui <you@example.com>
2025-02-15 14:56:13 -0700

    updated mmodels to have uuid and adding views

--- backend/pillpal/pillapp/views.py
+++ backend/pillpal/pillapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from models import User, Medication
+from serializer import UserSerializer, MedicationSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class MedicationViewSet(viewsets.ModelViewSet):
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
