33b2558cb96c10f4ef42bb4a04e717e53a61f639
dricmoy <dricbhattacharjee@gmail.com>
2025-02-15 15:28:27 -0700

    urls changed

--- backend/pillpal/pillapp/serializers.py
+++ backend/pillpal/pillapp/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
-from .models import Pill, User
+from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-
+"""
 class PillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pill
         fields = '__all__'
+"""
