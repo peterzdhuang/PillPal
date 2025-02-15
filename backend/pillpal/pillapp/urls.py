5a68e9b2093661f5f5fa0b6e4056ad0566c00f30
Daniel Cui <you@example.com>
2025-02-15 16:41:28 -0700

    Updated urls for consistency

--- backend/pillpal/pillapp/urls.py
+++ backend/pillpal/pillapp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from .views import AllMedicationsView, SingleMedicationView
+
 from . import views
 
 
 urlpatterns = [
     path('api/login/', views.UserAuthView.as_view(), name='login'),
     path('api/signup/', views.UserAuthView.as_view(), name='signup'),
+    # medication endpoints
+    path('api/<uuid:user_id>/medications', AllMedicationsView.as_view()),
+    path('api/<uuid:user_id>/medications/<uuid:medication_id>', SingleMedicationView.as_view())
 ]
--- backend/pillpal/pillapp/views.py
+++ backend/pillpal/pillapp/views.py
@@ -4,33 +4,6 @@ from django.shortcuts import get_object_or_404
 from .models import Medication
 from .serializer import MedicationSerializer
 
-<<<<<<< HEAD
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-from .models import User
-from .serializers import UserSerializer
-
-class UserAuthView(APIView):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, format=None):
-        content = {
-            'user': str(request.user),  
-            'auth': str(request.auth),  
-            'is_caregiver': request.user.is_caregiver,
-        }
-        return Response(content)
-
-    def post(self, request, *args, **kwargs):
-        user = User.objects.get(username=request.user)
-        user.is_caregiver = not user.is_caregiver
-        user.save()
-        return Response(status=200)
-    
-=======
 class AllMedicationsView(generics.ListCreateAPIView):
     """
     View to list all medications for a specific user or create a new one for that user.
@@ -65,4 +38,3 @@ class SingleMedicationView(generics.RetrieveUpdateDestroyAPIView):
         user_id = self.kwargs.get('user_id')
         medication_id = self.kwargs.get('medication_id')
         return get_object_or_404(Medication, id=medication_id, user_id=user_id)
->>>>>>> b1dd9b59f983113a5d45c23584cfa02a3f23c11c
