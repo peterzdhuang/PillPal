ff25a2727778163aee2b29a2ea163e6e8c6db008
Daniel Cui <you@example.com>
2025-02-15 17:12:42 -0700

    remove extra import

--- backend/pillpal/pillpal/urls.py
+++ backend/pillpal/pillpal/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 import pillapp.urls
 import pillapp
-import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
