56ef5e8a674d9a12c5405dcd3c07f46afe864de3
dricmoy <dricbhattacharjee@gmail.com>
2025-02-15 17:05:37 -0700

    bugfixes

--- backend/pillpal/pillapp/views.py
+++ backend/pillpal/pillapp/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from .models import Medication, User
-from .serializer import MedicationSerializer, UserSerializer
+from .serializers import MedicationSerializer, UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
