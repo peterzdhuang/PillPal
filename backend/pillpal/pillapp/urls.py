a5e6bd019b46cdd819f2b5196f983ece4d4a9095
Daniel Cui <you@example.com>
2025-02-15 20:15:18 -0700

    Added mock data and user medication endpoint

--- backend/pillpal/pillapp/urls.py
+++ backend/pillpal/pillapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('api/login/', views.LoginAuthView.as_view(), name='login'),
     path('api/signup/', views.UserAuthView.as_view(), name='signup'),
     path('api/users/', views.AllUsersView.as_view(), name='users'),
+    path('api/users/<user_id>/medications', views.UserMedication.as_view(), name='user medications')
 ]
